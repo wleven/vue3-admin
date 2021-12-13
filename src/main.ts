f36d27dd005bde1ca93eabacab6ce16b8f01cf89
郭超 <408493323@qq.com>
2021-12-13 10:43:03 +0800

    update 组件按需加载

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import Router from "@/router";
 
-import NaiveUI from "@/plugins/NaiveUI";
+// import NaiveUI from "@/plugins/NaiveUI";
 
 const app = createApp(App)
 app.use(Router)
-app.use(NaiveUI)
+// app.use(NaiveUI)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+// 按需加载
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        vueJsx(),
+        Components({
+            dts: true,
+            resolvers: [NaiveUiResolver()],
+        })
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src")
