fab66130439e467a34cf168b4bf51eda1a3db6d0
郭超 <408493323@qq.com>
2021-12-24 16:05:36 +0800

    feat 主题定制

---
+++ src/theme/form.ts
@@ -0,0 +1,6 @@
+import { GlobalThemeWithoutCommon } from 'naive-ui/es/config-provider/src/internal-interface';
+import { ExtractThemeOverrides } from 'naive-ui/lib/_mixins/use-theme';
+
+const ThemeForm: ExtractThemeOverrides<GlobalThemeWithoutCommon['Form']> = {};
+
+export default ThemeForm;
---
+++ src/theme/input.ts
@@ -0,0 +1,6 @@
+import { GlobalThemeWithoutCommon } from 'naive-ui/es/config-provider/src/internal-interface';
+import { ExtractThemeOverrides } from 'naive-ui/lib/_mixins/use-theme';
+
+const ThemeInput: ExtractThemeOverrides<GlobalThemeWithoutCommon['Input']> = {};
+
+export default ThemeInput;
