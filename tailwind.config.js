278a476df42046ab95a5509243d15e2532647609
郭超 <408493323@qq.com>
2021-12-24 16:06:11 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: '#18a058',
+      },
+    },
   },
   plugins: [],
   corePlugins: {
