b96917a5f9816d665af3ef26f56036580147c234
郭超 <408493323@qq.com>
2022-01-10 18:09:07 +0800

    update types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,21 @@
-import { MessageApiInjection } from 'naive-ui/lib/message/src/MessageProvider';
+import {
+  DialogProviderInst,
+  LoadingBarProviderInst,
+  MessageProviderInst,
+  NotificationProviderInst,
+} from 'naive-ui';
 
 declare global {
   interface Window {
-    $message: MessageApiInjection;
+    VConsole: any;
+    // naive ui Message组件
+    $NMessage: MessageProviderInst;
+    // naive ui LoadingBar组件
+    $NLoadingBar: LoadingBarProviderInst;
+    // naive ui Dialog组件
+    $NDialog: DialogProviderInst;
+    // naive ui Notification组件
+    $NNotify: NotificationProviderInst;
   }
 
   interface OBJ extends Object {
